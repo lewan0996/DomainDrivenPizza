f1ee3379f4e75d07fe3889e1b68115e20f8663f1
Marcin <marcin.lewandowski996@gmail.com>
2019-08-11 17:36:11 +0200

    Added repository classes

---
+++ src/Domain/Domain.SharedKernel/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Domain.SharedKernel
+{
+    public interface IRepository<T> where T: AggregateRoot
+    {
+        Task Add(T item);
+        Task<T> GetById(int id);
+    }
+}
---
+++ src/Infrastructure/Infrastructure.Shared/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Domain.SharedKernel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Shared
+{
+    public class Repository<T> : IRepository<T> where T : AggregateRoot
+    {
+        private readonly DbContext _dbContext;
+
+        public Repository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task Add(T item)
+        {
+            await _dbContext.Set<T>().AddAsync(item);
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+    }
+}
